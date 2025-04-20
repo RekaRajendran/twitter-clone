8bc6915c0aefa70017d9ea1ead542484995d45da
RekaRajendran <reka1901@gmail.com>
2025-04-20 18:16:19 +0530

    fix: db connection

--- backend/db/connectDb.js
+++ backend/db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB=async ()=>{
     try{
-        await mongoose.connect(process.env.Mongo_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("Mongo db connected") 
     }catch(error){
         console.log(`Error in connecting db: ${error}`)
